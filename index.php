a33168a7321437c0ad1e1396aa65e2f2780a9ea2
Luiza Brianes <luizabrianess04@gmail.com>
2024-02-28 21:05:29 -0300

    commit 01

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    function calcDobro(){
+        $dobro = $num *2;
+        return $dobro;
+    }
+    function calcTriplo($num){
+        $triplo = $num *3;
+        return $triplo;
+    }
+?>
